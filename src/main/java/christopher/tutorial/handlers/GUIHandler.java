cc688184f0046bb4888c66638f8ea1aa9dc1c99f
Christopher1993 <ChristopherJGamblin@Gmail.com>
2017-10-04 01:41:47 -0300

    Add files via upload

---
+++ src/main/java/christopher/tutorial/handlers/GUIHandler.java
@@ -0,0 +1,66 @@
+package christopher.tutorial.handlers;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import christopher.tutorial.Reference;
+import christopher.tutorial.Tutorial;
+import christopher.tutorial.container.ContainerCustomFurnace;
+import christopher.tutorial.gui.GuiCustomFurnace;
+import christopher.tutorial.tileentity.TileEntityCustomFurnace;
+
+public class GUIHandler implements IGuiHandler
+{
+	public GUIHandler()
+	{
+		NetworkRegistry.INSTANCE.registerGuiHandler(Tutorial.INSTANCE, this);
+	}
+	
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) 
+	{
+		TileEntity entity = world.getTileEntity(new BlockPos(x,y,z));
+		
+		if(entity != null)
+		{
+			switch(ID)
+			{
+			case Reference.GUI_CUSTOMFURNACE:
+				if(entity instanceof TileEntityCustomFurnace) 
+				{
+					return new ContainerCustomFurnace(player.inventory, (TileEntityCustomFurnace)entity);
+				}
+			}
+		}
+		
+		
+		return null;
+	}
+	
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) 
+	{
+		TileEntity entity = world.getTileEntity(new BlockPos(x,y,z));
+		
+		if(entity != null)
+		{
+			switch(ID)
+			{
+			case Reference.GUI_CUSTOMFURNACE:
+				if(entity instanceof TileEntityCustomFurnace) 
+				{
+					return new GuiCustomFurnace(player.inventory, (TileEntityCustomFurnace)entity);
+				}
+				
+				return null;
+			}
+		}
+		return null;
+	}
+	
+	public static void register()
+	{
+		NetworkRegistry.INSTANCE.registerGuiHandler(Tutorial.INSTANCE, new GUIHandler());
+	}	
+}
