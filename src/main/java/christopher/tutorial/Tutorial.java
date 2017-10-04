edc59ad193322c306c9b5c535c1153b450a7cad4
Christopher1993 <ChristopherJGamblin@Gmail.com>
2017-10-04 01:47:58 -0300

    Add files via upload

--- src/main/java/christopher/tutorial/Tutorial.java
+++ src/main/java/christopher/tutorial/Tutorial.java
@@ -1,23 +1,36 @@
 package christopher.tutorial;
 
-import christopher.tutorial.proxy.CommonProxy;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import christopher.tutorial.handlers.TileEntityHandler;
+import christopher.tutorial.proxy.CommonProxy;
+import christopher.tutorial.tabs.ChristopherTab;
 
 @Mod(modid =  Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class Tutorial 
 {
+	public static final CreativeTabs christophertab = new ChristopherTab("christophertab");
+	
 	@SidedProxy(clientSide = Reference.CLIENTPROXY, serverSide = Reference.COMMONPROXY)
 	public static CommonProxy proxy;
+	
+	@SideOnly(Side.CLIENT)
+	@Instance
+	public static Tutorial INSTANCE;
 	
 	@EventHandler
 	public static void preInit(FMLPreInitializationEvent event)
 	{
 		proxy.preInit(event);
+		TileEntityHandler.register();
 	}
 	
 	@EventHandler
