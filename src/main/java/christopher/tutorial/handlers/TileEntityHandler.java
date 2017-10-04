3527c67f02252bb8c9b6047e776d9ddc2554662b
Christopher1993 <ChristopherJGamblin@Gmail.com>
2017-10-04 01:42:39 -0300

    Add files via upload

---
+++ src/main/java/christopher/tutorial/handlers/TileEntityHandler.java
@@ -0,0 +1,14 @@
+package christopher.tutorial.handlers;
+
+import christopher.tutorial.tileentity.TileEntityCustomFurnace;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class TileEntityHandler 
+{
+		public static void register()
+		{
+			GameRegistry.registerTileEntity(TileEntityCustomFurnace.class, "custom_furnace_idle");
+		}
+
+}
