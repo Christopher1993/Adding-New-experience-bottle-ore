ef6257f4ce6235c274eb934177120cb964be6843
Christopher1993 <ChristopherJGamblin@Gmail.com>
2017-10-27 20:43:03 -0300

    Update RecipeHandler.java

--- src/main/java/christopher/tutorial/handlers/RecipeHandler.java
+++ src/main/java/christopher/tutorial/handlers/RecipeHandler.java
@@ -2,6 +2,8 @@
 package christopher.tutorial.handlers;
 
 import christopher.tutorial.init.BlockInit;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -11,5 +13,10 @@ public class RecipeHandler
 	public static void registerFurnaceRecipes()
 	{
 		GameRegistry.addSmelting(BlockInit.diamond_brick, new ItemStack(Items.DIAMOND), 100);
+		GameRegistry.addSmelting(BlockInit.experience_ore, new ItemStack(Items.EXPERIENCE_BOTTLE), 1000000);
+		GameRegistry.addSmelting(Items.WATER_BUCKET, new ItemStack(Items.LAVA_BUCKET), 100);
+		GameRegistry.addSmelting(Blocks.PLANKS, new ItemStack(Blocks.COBBLESTONE), 1000);
+		GameRegistry.addSmelting(Blocks.STONE, new ItemStack(Blocks.NETHERRACK), 1000);
+		GameRegistry.addSmelting(Items.WHEAT, new ItemStack(Blocks.DIRT), 0);
 	}
 }
