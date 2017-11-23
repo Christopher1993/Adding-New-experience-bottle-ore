ba4dd4982ce88bb29111a998a0a2dded0453f18a
Christopher1993 <ChristopherJGamblin@Gmail.com>
2017-11-22 20:46:39 -0400

    Create RedNetherBrickDoubleSlab.java

---
+++ src/main/java/christopher/tutorial/init/blocks/slab/RedNetherBrickDoubleSlab.java
@@ -0,0 +1,15 @@
+package christopher.tutorial.init.blocks.slab;
+
+public class RedNetherBrickDoubleSlab extends RedNetherBrickSlab
+{
+	public RedNetherBrickDoubleSlab(String name)
+	{
+		super(name);
+	}
+	
+	@Override
+	public boolean isDouble()
+	{
+		return true;
+	}
+}
