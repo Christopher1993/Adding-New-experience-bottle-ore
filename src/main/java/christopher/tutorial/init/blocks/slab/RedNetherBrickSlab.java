2dfba06ccce33ced77d42de357cfbe2dbce81af2
Christopher1993 <ChristopherJGamblin@Gmail.com>
2017-11-22 20:43:36 -0400

    Create RedNetherBrickSlab.java

---
+++ src/main/java/christopher/tutorial/init/blocks/slab/RedNetherBrickSlab.java
@@ -0,0 +1,94 @@
+package christopher.tutorial.init.blocks.slab;
+
+import java.util.Random;
+
+import christopher.tutorial.init.BlockInit;
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockSlab.EnumBlockHalf;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class RedNetherBrickSlab extends BlockSlab 
+{
+	public RedNetherBrickSlab(String name)
+	{
+		super(Material.ROCK);
+		this.blockSoundType = SoundType.STONE;
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		
+		IBlockState state = this.blockState.getBaseState();
+		
+		if(!this.isDouble())
+		{
+			state = state.withProperty(HALF, EnumBlockHalf.BOTTOM);
+		}
+		
+		setDefaultState(state);
+		this.useNeighborBrightness = true;
+	}
+	
+	@Override
+	public String getUnlocalizedName(int meta) 
+	{
+		return this.getUnlocalizedName();
+	}
+	
+	@Override
+	public IProperty<?> getVariantProperty() 
+	{
+		return HALF;
+	}
+
+	@Override
+	public Comparable<?> getTypeForItem(ItemStack stack) 
+	{
+		return EnumBlockHalf.BOTTOM;
+	}
+	
+	@Override
+	public int damageDropped(IBlockState state) 
+	{
+		return 0;
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) 
+	{
+		if(!this.isDouble())
+		{
+			return this.getDefaultState().withProperty(HALF, EnumBlockHalf.values()[meta % EnumBlockHalf.values().length]);
+		}
+		return this.getDefaultState();
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) 
+	{
+		if(this.isDouble())
+		{
+			return 0;
+		}
+		
+		return ((EnumBlockHalf)state.getValue(HALF)).ordinal() + 1;
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) 
+	{
+		return Item.getItemFromBlock(BlockInit.red_nether_brick_slab);
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() 
+	{
+		return new BlockStateContainer(this, new IProperty[] {HALF});
+	}
+
+	}
+}
