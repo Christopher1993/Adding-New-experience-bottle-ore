dd889ad050ee75dd7c6910d352a79a0f7a67e44a
Christopher1993 <ChristopherJGamblin@Gmail.com>
2018-01-12 15:12:16 -0400

    Add files via upload

---
+++ src/main/java/christopher/tutorial/handlers/LootTableHandler.java
@@ -0,0 +1,10 @@
+package christopher.tutorial.handlers;
+
+import christopher.tutorial.Reference;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootTableList;
+
+public class LootTableHandler 
+{
+	public static final ResourceLocation DIAMONDBRICKGOLEM = LootTableList.register(new ResourceLocation(Reference.MODID, "diamond_brick_golem"));
+}
